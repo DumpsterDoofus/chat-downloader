632d8ac64d58cfd6ddb832105a241e1ae99ccbba
DumpsterDoofus <dumpsterdoofus@gmail.com>
2021-10-09 08:47:34 -0400

    Subtitle downloads

--- srt_subtitle_downloader.py
+++ srt_subtitle_downloader.py
@@ -16,11 +16,13 @@ class SrtLine:
     Index: int
     StartTimeSeconds: float
     EndTimeSeconds: float
+    Author: str
     MessageText: str
-    def __init__(self, index: int, start_time_seconds: float, end_time_seconds: float, message_text: str) -> None:
+    def __init__(self, index: int, start_time_seconds: float, end_time_seconds: float, author: str, message_text: str) -> None:
         self.Index = index
         self.StartTimeSeconds = start_time_seconds
         self.EndTimeSeconds = end_time_seconds
+        self.Author = author
         self.MessageText = message_text
     def __seconds_to_timestamp(self, seconds: float):
         int_seconds = int(seconds)
@@ -29,7 +31,42 @@ class SrtLine:
         milliseconds = round(1000 * (float(seconds) - int_seconds))
         return f"{'-' if seconds < 0 else ''}{h:02}:{m:02}:{s:02},{milliseconds:03}"
     def to_string(self) -> str:
-        return f'{self.Index}\n{self.__seconds_to_timestamp(self.StartTimeSeconds)} --> {self.__seconds_to_timestamp(self.EndTimeSeconds)}\n{self.MessageText}\n\n'
+        return f'{self.Index}\n{self.__seconds_to_timestamp(self.StartTimeSeconds)} --> {self.__seconds_to_timestamp(self.EndTimeSeconds)}\n<font color="#00FF00">{self.Author}</font>: {self.MessageText}\n\n'
+
+assHeader = """[Script Info]
+ScriptType: v4.00+
+Collisions: Normal
+PlayResX: 640
+PlayResY: 480
+Timer: 100.0000
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Myriad Web Pro Condensed,26,&H00ffffff,&H0000ffff,&H0025253a,&H96000000,0,0,0,0,100,100,0,0.00,1,2,1,2,15,15,20,1
+
+[Events]
+Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text
+"""
+
+class AssLine:
+    StartTimeSeconds: float
+    EndTimeSeconds: float
+    Author: str
+    MessageText: str
+    def __init__(self, start_time_seconds: float, end_time_seconds: float, author: str, message_text: str) -> None:
+        self.StartTimeSeconds = start_time_seconds
+        self.EndTimeSeconds = end_time_seconds
+        self.Author = author
+        self.MessageText = message_text
+    def __seconds_to_timestamp(self, seconds: float):
+        int_seconds = int(seconds)
+        h, remainder = divmod(abs(int_seconds), 3600)
+        m, s = divmod(remainder, 60)
+        hundredths = round(100 * (float(seconds) - int_seconds))
+        return f"{'-' if seconds < 0 else ''}{h:01}:{m:02}:{s:02}.{hundredths:02}"
+    def to_string(self) -> str:
+        fadeMilliseconds = round(1000 * (self.EndTimeSeconds - self.StartTimeSeconds) / 20)
+        return f'Dialogue: 0,{self.__seconds_to_timestamp(self.StartTimeSeconds)},{self.__seconds_to_timestamp(self.EndTimeSeconds)},,,0000,0000,0000,,{{\\move(320,480,320,360)}}{{\\fad({fadeMilliseconds},{fadeMilliseconds})}}{{\\1c&H00FF00&}}{self.Author}: {{\\1c&HFFFFFF&}}{self.MessageText}\n'
 
 def even_spaced_timestamp_filter(chat_messages: List[ChatMessage], smoothing_interval_seconds: float = 5):
     """Smooths out chat message timestamps within regularly-spaced intervals, so that timestamps are more evenly-spaced. This helps readability when bursts of several messages occur at nearly the same time."""
@@ -84,9 +121,43 @@ def parse_srt_lines(chat_messages: List[ChatMessage], max_seconds_onscreen: floa
             index=index,
             start_time_seconds=chatMessage.TimestampSeconds,
             end_time_seconds=min(nextTimestampSeconds, chatMessage.TimestampSeconds + max_seconds_onscreen),
-            message_text=f'<font color="#00FF00">{chatMessage.Author}</font>: {chatMessage.MessageText}'))
+            author=chatMessage.Author,
+            message_text=chatMessage.MessageText))
     return srtLines
 
+def parse_ass_lines(chat_messages: List[ChatMessage], max_seconds_onscreen: float = 5, grouping_interval_seconds: float = 5, max_subtitles_onscreen: int = 5) -> List[AssLine]:
+    if max_seconds_onscreen <= 0:
+        raise ValueError(f'max_seconds_onscreen must be positive, but was {max_seconds_onscreen}')
+    if grouping_interval_seconds <= 0:
+        raise ValueError(f'grouping_interval_seconds must be positive, but was {grouping_interval_seconds}')
+    if max_subtitles_onscreen <= 0:
+        raise ValueError(f'max_subtitles_onscreen must be positive, but was {max_seconds_onscreen}')
+    assLines: List[AssLine] = []
+    if len(chat_messages) == 0:
+        return assLines
+    minTimestamp = 0
+    maxTimestamp = grouping_interval_seconds
+    lastTimestamp = chat_messages[-1].TimestampSeconds
+    minIndex = 0
+    maxIndex = -1
+    while minTimestamp < lastTimestamp:
+        while maxIndex + 1 < len(chat_messages) and chat_messages[maxIndex + 1].TimestampSeconds < maxTimestamp:
+            maxIndex += 1
+        commentsInInterval = maxIndex - minIndex + 1
+        if commentsInInterval > 0:
+            subtitlesPerSecond = commentsInInterval / grouping_interval_seconds
+            for i in range(0, commentsInInterval):
+                chatMessage = chat_messages[minIndex + i]
+                timeOnscreen = min(max_subtitles_onscreen / subtitlesPerSecond, max_seconds_onscreen)
+                assLines.append(AssLine(
+                    start_time_seconds=chatMessage.TimestampSeconds,
+                    end_time_seconds=chatMessage.TimestampSeconds + timeOnscreen,
+                    author=chatMessage.Author,
+                    message_text=chatMessage.MessageText))
+        minIndex = maxIndex + 1
+        minTimestamp += grouping_interval_seconds
+        maxTimestamp += grouping_interval_seconds
+    return assLines
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -94,14 +165,26 @@ if __name__ == '__main__':
     parser.add_argument('--max_seconds_onscreen', required=False, default=5)
     parser.add_argument('--smoothing_interval_seconds', required=False, default=5)
     parser.add_argument('--title', required=False, default='subtitles')
+
+    subparsers = parser.add_subparsers(dest='command', required=True)
+    parser_srt = subparsers.add_parser('srt')
+    parser_ass = subparsers.add_parser('ass')
+    parser_ass.add_argument('--max_subtitles_onscreen', required=False, default = 5)
+
     args = parser.parse_args()
 
     chatMessages = parse_chat_messages(ChatDownloader().get_chat(args.url))
     even_spaced_timestamp_filter(chatMessages, args.smoothing_interval_seconds)
-    srtLines = parse_srt_lines(chatMessages, args.max_seconds_onscreen)
+    
+    if args.command == 'srt':
+        lines = parse_srt_lines(chatMessages, args.max_seconds_onscreen)
+    elif args.command == 'ass':
+        lines = parse_ass_lines(chatMessages, args.max_seconds_onscreen, args.smoothing_interval_seconds, args.max_subtitles_onscreen)
 
-    srtPath = os.path.join(os.getcwd(), f'{args.title}.srt')
-    with open(srtPath, 'w', encoding='utf-8') as srtFile:
-        for srtLine in srtLines:
-            srtFile.write(srtLine.to_string())
-        print(f'Wrote SRT subtitles to {srtPath}')
+    filePath = os.path.join(os.getcwd(), f'{args.title}.{args.command}')
+    with open(filePath, 'w', encoding='utf-8') as file:
+        if args.command == 'ass':
+            file.write(assHeader)
+        for line in lines:
+            file.write(line.to_string())
+        print(f'Wrote subtitles to {filePath}')
